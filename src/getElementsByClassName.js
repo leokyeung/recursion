7d293c042b6d578166209f759bfe42cd53fa09f3
Leo Yeung <leoyeung99@gmail.com>
2019-10-05 21:44:25 -0700

    updated

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,20 +3,20 @@
 //   return document.getElementsByClassName(className);
 // };
 
-var getElementsByClassName = function(className) {
+var getElementsByClassName = function (className) {
   var domBody = document.body;
   var output = [];
 
-  function check (node) {
+  function check(node) {
     if (node.classList !== undefined && node.classList.contains(className)) {
       output.push(node);
     }
-
-      for ( var i = 0; i< node.childNodes.length; i++){
-        check(node.childNodes[i]);
-      }
+    for (var i = 0; i < node.childNodes.length; i++) {
+      check(node.childNodes[i]);
+    }
 
   }
+
   check(domBody);
   return output;
 };
